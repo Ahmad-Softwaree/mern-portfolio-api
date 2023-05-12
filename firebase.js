09288dca706fe43b53eb1aa9b2f07482c3cf48b4
Ahmad Software <dr.ahmad.salah.54@gmail.com>
2023-05-13 02:01:08 +0300

    change env variables

---
+++ firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+import dotenv from "dotenv";
+dotenv.config();
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGEIN_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
+
+initializeApp(firebaseConfig);
+
+const firebaseStorage = getStorage();
+
+export default firebaseStorage;
